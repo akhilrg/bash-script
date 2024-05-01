0950ede92db506c0197ef427732e488ef0d18256
akhilrg <akhil.gujja6415@gmail.com>
2024-05-01 18:07:22 +0000

    time based greeting added

--- echo.sh
+++ echo.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 
-echo "Enter your name"
+h='date +%H'
+if [ $h -lt 12]; then
+greeting= "Good Morning"
+elif [ $h -lt 18 ]; then
+greeting="Good Afternoon"
+else
+greeting="Good Evening"
+fi
+	
+echo "Hi, What is your name?"
 read name
 
-echo "Hi How's it going $name ?"
+echo "$greeting $name, It's nice to meet you."
+
+
 
 
